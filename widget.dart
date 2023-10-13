de3d79277838e7fad999a256c019a9fefe5e53cc
NyimasNisrinaaKamilah-2226250064 <nyimasnisrinaakamilah_2226250064@mhs.mdp.ac.id>
2023-10-13 18:40:22 +0700

    Menambahkan icon pada AppBar

--- widget.dart
+++ widget.dart
@@ -8,7 +8,27 @@ class MyApp extends StatelessWidget {
     const MyApp({super.key});
 
     @override
-    Widget build(BuildContext context){
-        return const Placeholder();
+   Widget build (BuildContext context){
+        return MaterialApp(
+            title: "Hallo Widget",
+            theme: ThemeData(primarySwatch: Colors.blue),
+            home: firstScreen()
+        );
+    }
+}
+
+    class firstScreen extends StatelessWidget {
+        const firstScreen({super.key});
+
+        @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+            title: const Text("Nyimas Nisrinaa Kamilah"),
+            ),
+            body: const Center(
+                child: Text("Hallo Nyimas"),
+            ),
+        );
     }
 }
