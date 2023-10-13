c6f9965114b6ade640ad11ebe9d606e8a383b9a2
NyimasNisrinaaKamilah-2226250064 <nyimasnisrinaakamilah_2226250064@mhs.mdp.ac.id>
2023-10-13 18:39:42 +0700

    Pisah widget

--- widget.dart
+++ widget.dart
@@ -8,27 +8,7 @@ class MyApp extends StatelessWidget {
     const MyApp({super.key});
 
     @override
-    Widget build (BuildContext context){
-        return MaterialApp(
-            title: "Hallo Widget",
-            theme: ThemeData(primarySwatch: Colors.blue),
-            home: firstScreen()
-        );
-    }
-}
-
-    class firstScreen extends StatelessWidget {
-        const firstScreen({super.key});
-
-        @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-            title: const Text("Nyimas Nisrinaa Kamilah"),
-            ),
-            body: const Center(
-                child: Text("Hallo Nyimas"),
-            ),
-        );
+    Widget build(BuildContext context){
+        return const Placeholder();
     }
 }
