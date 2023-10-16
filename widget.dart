cbeb3e43f0da76a9f54de492ddd9c3b23da66380
NyimasNisrinaaKamilah-2226250064 <nyimasnisrinaakamilah_2226250064@mhs.mdp.ac.id>
2023-10-16 18:43:34 +0700

    latihan

--- widget.dart
+++ widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(){
-    runApp(const myApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,16 +17,17 @@ class MyApp extends StatelessWidget {
     }
 }
 
-    class firstScreen extends StatelessWidget {
+class firstScreen extends StatelessWidget {
         const firstScreen({super.key});
 
         @override
         Widget build(BuildContext context) {
         return Scaffold(
             appBar: AppBar(
-            title: const Text("Nyimas Nisrinaa Kamilah"),
+            title: const Text("Widget Demo"),
             actions: <Widget>[
-                UconButton(onPressed: (){}, icon:const Icon(
+                IconButton(onPressed: (){},
+                 icon:const Icon(
                     Icons.search,
                     color:Colors.white,
                 ))
@@ -42,6 +43,62 @@ class MyApp extends StatelessWidget {
             body: const Center(
                 child: Text("Hallo Nyimas"),
             ),
-        );
-    }
+    );
+  }
+}
+   
+class constDemo extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: const Text('Widget Demo'),
+      ),
+      body: Column(
+        children: [
+            Container(
+                  height: 200,
+                  margin: const EdgeInsets.all(16),
+                  padding: const EdgeInsets.all(16),
+                  color : Colors.blue,
+                  child: const Center(
+                    child: Text(
+                      'Ini adalah contoh penggunaan Container',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    textAlign: TextAlign.center,
+                    ),
+                ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text('TombolElevated'),
+                ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.amber),
+                Text('Rating: 4.5'),
+              ],
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.network(
+                  'https://picsum.photos/id/7/300/200',
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+        ],
+      ),
+     );
+  }
 }
